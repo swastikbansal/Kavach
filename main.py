dcb546cedcad165e6193aa43c19fa26ce3d45ab5
Swastik Bansal <swastikbansal0@gmail.com>
2023-07-14 17:28:35 +0530

    Kivy Progress

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from kivy.app import App
+from kivy.metrics import dp
+from kivy.properties import StringProperties
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 
+class WidgetExample(GridLayout):
+    def on_button_click(self):
+        print("Yes !")
 
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.orientation = "lr-tb"   
+        for i in range(100):
+            # size = dp(100 + i*10)
+            size = dp(100)
+            b = Button(text = str(i+1),size_hint = (None,None), size = (size,size))
+            self.add_widget(b)
 
 class AnchorLayoutExample(AnchorLayout):
     pass
@@ -26,8 +43,8 @@ class BoxLayoutExample(BoxLayout):
         self.add_widget(b3)
 """
     
-# class MainWidget(Widget):
-#     pass
+class MainWidget(Widget):
+    pass
 
 class KavachApp(App):
     pass
