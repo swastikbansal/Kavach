119c9d00e45dd24c66975cbb114e1bd6e8a9d85d
Swastik Bansal <swastikbansal0@gmail.com>
2023-07-14 20:02:57 +0530

    Updated Line 2 Main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.metrics import dp
-from kivy.properties import StringProperties
+from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
@@ -9,8 +9,14 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 
 class WidgetExample(GridLayout):
+    counter = 0
+    my_text = StringProperty(str(counter))
     def on_button_click(self):
-        print("Yes !")
+        self.counter +=  1
+        self.my_text = str(self.counter)
+    
+    def on_state(self,state):
+        print("Toggle State : ",state.state)
 
 
 class StackLayoutExample(StackLayout):
