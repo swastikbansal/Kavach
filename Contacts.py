031a3b622ce221f38552e73129b6a28e22462abd
Swastik Bansal <swastikbansal0@gmail.com>
2023-07-16 22:03:55 +0530

    Added Contacts.py

---
+++ Contacts.py
@@ -0,0 +1,53 @@
+import csv
+
+# CSV File Class
+class Conacts:
+    def __init__(self):
+        # Creating a new empty CSV File and adding headers at first row
+        self.file = open("EmergencyContacts.csv", "a+")
+
+    # Writing Data into CSV File
+    def writeFile(self,name,number):
+        
+        self.writeData = [name,number]  
+
+        #Writing data
+        with self.file :
+            writer = csv.writer(self.file)
+            writer.writerow(self.writeData)
+
+    # Function for reading data from file
+    def readFile(self):
+        self.file.seek(0)
+        self.readData = csv.reader(self.file)
+        self.data_list = []
+
+        for rows in self.readData:
+            self.data_list.append(rows)
+
+        print(self.data_list)
+
+        return self.data_list
+
+
+
+
+    # if mobileNumber.isnumeric():
+    #     # Writing to CSV file
+    #     with CSVFileObject.file:
+    #         CSVFileObject.writeFile(contactsList)
+    #         print("Data Updated Successfully !")
+
+    #         print("Current Contacts in File\n", CSVFileObject.readFile())
+
+    #         # Asking user if he wants to add more contacts or not
+    #         choice = input("Do you want to add more contacts(y/n) : ")
+
+    #         if choice.lower() == "y":
+    #             continue
+
+    #         else:
+    #             break
+
+    # else:
+    #     print("Invalid Mobile number")
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen,ScreenManager
+
 import csv
 
+import Contacts
+
+contacts = Contacts.Conacts()
 
 #---------------- GUI ---------------------
 class MainWin(Screen):
@@ -18,17 +22,12 @@ class AddEmergencyContacts(Screen):
     def numberTextValidate(self,Widget):
         self.number = Widget.text 
     
-    # Writing Data into CSV File
-    def writeFile(self,nameInput,numberInput):
+    def writeCSVFile(self,nameInput,numberInput):
         #Reading Input from both the text Box 
         self.name = nameInput.text 
-        self.number = numberInput.text 
+        self.number = numberInput.text
 
-        self.writeData = [self.name,self.number]
-
-        #Writing data
-        writer = csv.writer(open("EmergencyContacts.csv", "a+"))
-        writer.writerow(self.writeData)
+        contacts.writeFile(self.name,self.number)
 
 class VeiwEmergencyContacts(Screen):
     pass
@@ -47,53 +46,3 @@ class KavachApp(App):
 KavachApp().run()
 
 
-# CSV File Class
-class CSVFile:
-    def __init__(self):
-        # Creating a new empty CSV File and adding headers at first row
-        self.file = open("EmergencyContacts.csv", "a+")
-
-    # Function for reading data from file
-    def readFile(self):
-        self.file.seek(0)
-        self.readData = csv.reader(self.file)
-        self.data_list = []
-
-        for rows in self.readData:
-            self.data_list.append(rows)
-
-        print(self.data_list)
-
-        return self.data_list
-
-   
-
-
-CSVFileObject = CSVFile()
-
-while True:
-    # Inputting emegency contacts data
-    name = input("Enter the name of the contact : ")
-    mobileNumber = input("Enter the Contact Number : ")
-
-    contactsList = [name, mobileNumber]
-
-    if mobileNumber.isnumeric():
-        # Writing to CSV file
-        with CSVFileObject.file:
-            CSVFileObject.writeFile(contactsList)
-            print("Data Updated Successfully !")
-
-            print("Current Contacts in File\n", CSVFileObject.readFile())
-
-            # Asking user if he wants to add more contacts or not
-            choice = input("Do you want to add more contacts(y/n) : ")
-
-            if choice.lower() == "y":
-                continue
-
-            else:
-                break
-
-    else:
-        print("Invalid Mobile number")
