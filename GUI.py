c2119584fbc6de5bc572d0ef84aaf82a37314a9d
Swastik Bansal <swastikbansal0@gmail.com>
2023-07-23 11:38:18 +0530

    Commenting in the current Work

--- GUI.py
+++ GUI.py
@@ -1,24 +1,21 @@
+#Kivy Libraries
 from kivy.app import App
 from kivy.properties import StringProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
 
+#Local file imports
 import Contacts
 
-if __name__ == "__main__":
-    contacts = Contacts.Contacts()
-
-    #Global Variables
-    # globalName = StringProperty("")
-    # globalNumber = StringProperty("")
-    name_list = []
-    number_list = []
 
 # ---------------- GUI ---------------------
 class MainWin(Screen):
     pass
 
 
+#Emergency Window Menu Class 
 class EmergencyContacts(Screen):
+
+    #This function is executes on press of Veiw button to extract data from Contacts 
     def veiwButton(self):
         global name_list,number_list
 
@@ -30,15 +27,18 @@ class EmergencyContacts(Screen):
                 number_list.append(row[1])
 
 
+#Add Contacts Class for GUI
 class AddEmergencyContacts(Screen):
 
+    #Functions for Updating Data on Text Validation
     def nameTextValidate(self,Widget):
         self.addName = Widget.text 
     
     def numberTextValidate(self,Widget):
         self.addNumber = Widget.text 
     
-    def writeContact(self,nameInput,numberInput):
+    #Updating data on pressing of Button
+    def addContactButton(self,nameInput,numberInput):
         #Reading Input from both the text Box 
         self.addName = nameInput.text 
         self.addNumber = numberInput.text
@@ -46,13 +46,16 @@ class AddEmergencyContacts(Screen):
         contacts.writeFile(self.addName, self.addNumber)
 
 
+#Veiw Contacts Screen Class and integration with backend
 class ViewEmergencyContacts(Screen):
     veiwName = StringProperty("") 
     veiwNumber = StringProperty("") 
-
+    
+    #Function executes as soon as Screen opens
     def on_enter(self, *args):
         global name_list, number_list 
 
+        #Temp var for name and number as kivy StringProperty dosen't support concatenation
         tempName = ""
         tempNumber = ""
 
@@ -65,18 +68,15 @@ class ViewEmergencyContacts(Screen):
                 self.veiwName = i
                 self.veiwNumber = j
 
+        #Assigning final values to String Property
         self.veiwName += tempName    
         self.veiwNumber += tempNumber
-        
-
-
-
-
+    
 
 class DeleteEmergencyContacts(Screen):
     pass
 
-
+#Screen Manager class for managing multiple Classes
 class ScreenManager(ScreenManager):
     pass
 
@@ -85,4 +85,14 @@ class KavachApp(App):
     pass
 
 
-KavachApp().run()
+#__main__
+if __name__ == "__main__":
+    contacts = Contacts.Contacts()
+
+    #Global Variables
+    # globalName = StringProperty("")
+    # globalNumber = StringProperty("")
+    name_list = []
+    number_list = []
+
+    KavachApp().run()
