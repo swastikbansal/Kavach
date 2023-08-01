671d98d1d6fc9a54100138c28960c31f45c7ff95
Swastik Bansal <swastikbansal0@gmail.com>
2023-08-01 21:39:47 +0530

    Emergency Contacts Successfully integrated.

---
+++ app/src/main/java/com/example/kavach/item_contact.java
@@ -0,0 +1,14 @@
+package com.example.kavach;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class item_contact extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_contact);
+    }
+}
