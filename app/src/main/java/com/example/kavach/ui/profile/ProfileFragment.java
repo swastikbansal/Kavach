55745c5d6a8fa153ea7bcd8714268cc85ba95da7
Prabhjot Singh Assi <prabhjotassi16@gmail.com>
2023-08-02 20:29:02 +0530

    Emergency Contacts Window Updated. New Profile Fragment added. Icons added.

---
+++ app/src/main/java/com/example/kavach/ui/profile/ProfileFragment.java
@@ -0,0 +1,24 @@
+package com.example.kavach.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.kavach.R;
+import com.example.kavach.databinding.FragmentHomeBinding;
+import com.example.kavach.ui.home.HomeViewModel;
+
+public class ProfileFragment extends Fragment{
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
