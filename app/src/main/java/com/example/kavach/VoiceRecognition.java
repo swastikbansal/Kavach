517c11e83e0c4839cf79fed1ac16172d3315a278
vidit sharma <vidit.awesome2305@gmail.com>
2023-08-11 18:07:55 +0530

    Speech recognition 1st Draft Completed.

--- app/src/main/java/com/example/kavach/VoiceRecognition.java
+++ app/src/main/java/com/example/kavach/VoiceRecognition.java
@@ -1,9 +1,12 @@
 package com.example.kavach;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
-
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -11,17 +14,10 @@ import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.Spinner;
-import android.content.ActivityNotFoundException;
-import android.speech.RecognizerIntent;
-import android.widget.Switch;
 import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.Locale;
-import android.content.SharedPreferences;
-import android.content.Intent;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import pl.droidsonroids.gif.GifImageView;
 
@@ -33,11 +29,32 @@ public class VoiceRecognition extends AppCompatActivity {
     //Animations
     Animation speechAnimation;
 
-    private static final int REQUEST_CODE_SPEECH_INPUT = 100;
-    private static final String TRIGGER_WORD_KEY = "trigger_word";
-    private static final String DEFAULT_TRIGGER_WORD = "shield";
-    private SwitchCompat switchStatus;
-    private boolean isSpeechRecognitionEnabled = false;
+    private Button startButton;
+    private Button stopButton;
+    private boolean isWakeWordServiceRunning = false;
+
+    String triggerWordKey = Constants.TRIGGER_WORD_KEY;
+    String defaultTriggerWord = Constants.DEFAULT_TRIGGER_WORD;
+
+    private LocationHandler locationHandler;
+
+
+    private BroadcastReceiver wakeWordReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if ("WAKE_WORD_DETECTED".equals(intent.getAction())) {
+                int keywordIndex = intent.getIntExtra("keywordIndex", -1);
+                String selectedWakeWord = intent.getStringExtra("selectedWakeWord");
+
+                if (keywordIndex == 0) {
+                    showToast("Wake word detected: " + selectedWakeWord);
+                    locationHandler.getCurrentLocation();
+                } else {
+                    showToast("Detected wake word does not match selected wake word");
+                }
+            }
+        }
+    };
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -51,31 +68,7 @@ public class VoiceRecognition extends AppCompatActivity {
         voice = findViewById(R.id.gifsr);
         voice.setAnimation(speechAnimation);
 
-        // Initialize the Switch widget
-        switchStatus = findViewById(R.id.switch1);
-        switchStatus.setChecked(false); // Default state is "disabled"
-        switchStatus.setText("Disabled    "); // Default text
 
-        switchStatus.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                isSpeechRecognitionEnabled = isChecked;
-                switchStatus.setText(isChecked ? "Enabled    " : "Disabled    ");
-            }
-        });
-
-        Button btnSpeechRecognition = findViewById(R.id.btnSpeechRecognition);
-        btnSpeechRecognition.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isSpeechRecognitionEnabled) {
-                    startSpeechRecognition();
-                    Toast.makeText(VoiceRecognition.this, "Work is in Prototype Stage. Still Developing.", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(VoiceRecognition.this, "Speech recognition is disabled. Enable the switch to use it.", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
 
         Spinner spinnerDropdown = findViewById(R.id.spinnerDropdown);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
@@ -94,62 +87,104 @@ public class VoiceRecognition extends AppCompatActivity {
                 // Save the selected item to SharedPreferences as lowercase
                 SharedPreferences sharedPreferences = getSharedPreferences("my_prefs", MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString(TRIGGER_WORD_KEY, selectedItem.toLowerCase());
+                editor.putString(Constants.TRIGGER_WORD_KEY, selectedItem.toLowerCase());
                 editor.apply();
+
+                // Stop the current wake word service if running
+                if (isWakeWordServiceRunning) {
+                    stopWakeWordService();
+                    startWakeWordService();
+                }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-                // Handle the case when nothing is selected (optional)
+                // Handle the case when nothing is selected (if needed)
             }
         });
+
+
+        // Initialize buttons
+        startButton = findViewById(R.id.startButton);
+        stopButton = findViewById(R.id.stopButton);
+
+        // Set click listeners for buttons
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isWakeWordServiceRunning) {
+                    startWakeWordService();
+                }
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isWakeWordServiceRunning) {
+                    stopWakeWordService();
+                }
+            }
+        });
+
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter("WAKE_WORD_DETECTED");
+        registerReceiver(wakeWordReceiver, filter);
+
+        locationHandler = new LocationHandler(this, new LocationHandler.LocationListener() {
+            @Override
+            public void onLocationChanged(double latitude, double longitude) {}
+
+            @Override
+            public void onLocationChanged(Location location) {
+                // Implement this if needed
+            }
+        });
+
     }
 
-    private void startSpeechRecognition() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Unregister the BroadcastReceiver
+        unregisterReceiver(wakeWordReceiver);
+    }
 
-        try {
-            // Start the speech recognition activity
-            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
-        } catch (ActivityNotFoundException e) {
-            // Speech recognition not supported on this device
-            Toast.makeText(this, "Speech recognition is not supported on this device.", Toast.LENGTH_SHORT).show();
+    private void startWakeWordService() {
+        if (!isWakeWordServiceRunning) {
+
+            // Start the wake word service
+            Intent serviceIntent = new Intent(this, WakeWordService.class);
+            startService(serviceIntent);
+
+            // Disable start button and enable stop button
+            startButton.setEnabled(false);
+            stopButton.setEnabled(true);
+            isWakeWordServiceRunning = true;
         }
     }
+
+    private void stopWakeWordService() {
+        Intent serviceIntent = new Intent(this, WakeWordService.class);
+        stopService(serviceIntent);
+
+        // Enable start button and disable stop button
+        startButton.setEnabled(true);
+        stopButton.setEnabled(false);
+        isWakeWordServiceRunning = false;
+    }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == REQUEST_CODE_SPEECH_INPUT && resultCode == RESULT_OK && data != null) {
-            // Get the recognized speech results
-            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 
-            if (result != null && result.size() > 0) {
-                // Process the recognized speech result
-                String recognizedText = result.get(0);
+    }
 
-                // Get the trigger word from SharedPreferences with default value
-                SharedPreferences sharedPreferences = getSharedPreferences("my_prefs", MODE_PRIVATE);
-                String triggerWord = sharedPreferences.getString(TRIGGER_WORD_KEY, DEFAULT_TRIGGER_WORD).toLowerCase();
-
-                // Check if the recognized text contains the trigger word
-                if (!triggerWord.isEmpty() && recognizedText.toLowerCase().contains(triggerWord)) {
-
-                    // Call the sendSMS() method from SmsHandler to send the SMS
-                    SmsHandler.sendSMS(this, "Hi, the sender has selected you as their Emergency contact. In time of future crisis, an SOS help message will be sent to you.");
-                    // Trigger word is recognized, indicating that using toast
-                    Toast.makeText(this, "Recognized Successful", Toast.LENGTH_SHORT).show();
-
-
-                } else {
-                    // Trigger word is not recognized
-                    Toast.makeText(this, "Unable to Recognize. Try Again!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
 
