1dc28e9167d0fd8fd30270ca0d39e772ffd62710
vidit sharma <vidit.awesome2305@gmail.com>
2023-08-11 18:11:12 +0530

    Speech recognition Completed.

---
+++ app/src/main/java/com/example/kavach/Constants.java
@@ -0,0 +1,6 @@
+package com.example.kavach;
+
+public class Constants {
+    public static final String TRIGGER_WORD_KEY = "trigger_word";
+    public static final String DEFAULT_TRIGGER_WORD = "shield";
+}
---
+++ app/src/main/java/com/example/kavach/WakeWordService.java
@@ -0,0 +1,127 @@
+package com.example.kavach;
+
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import ai.picovoice.porcupine.Porcupine;
+import ai.picovoice.porcupine.PorcupineException;
+import ai.picovoice.porcupine.PorcupineManager;
+import ai.picovoice.porcupine.PorcupineManagerCallback;
+
+
+
+public class WakeWordService extends Service {
+
+    private PorcupineManager porcupineManager;
+    private static final int NOTIFICATION_ID = 123;
+
+    String triggerWordKey = Constants.TRIGGER_WORD_KEY;
+    String defaultTriggerWord = Constants.DEFAULT_TRIGGER_WORD;
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        startForeground(NOTIFICATION_ID, createNotification());
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        setupWakeWordDetection();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        stopWakeWordDetection();
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private Notification createNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "WakeWordChannel";
+            String channelName = "Wake Word Detection";
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        return new NotificationCompat.Builder(this, "WakeWordChannel")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+    }
+
+    private void setupWakeWordDetection() {
+        Context context = this;
+
+        SharedPreferences sharedPreferences = getSharedPreferences("my_prefs", MODE_PRIVATE);
+        String selectedWakeWord = sharedPreferences.getString(Constants.TRIGGER_WORD_KEY, Constants.DEFAULT_TRIGGER_WORD);
+
+        Porcupine.BuiltInKeyword[] keywords = new Porcupine.BuiltInKeyword[]{
+                Porcupine.BuiltInKeyword.valueOf(selectedWakeWord.toUpperCase())
+        };
+
+        try {
+            porcupineManager = new PorcupineManager.Builder()
+                    .setAccessKey("nnlklAGraQk1b9da+VcyK+r/q9bWUFU3mf5vq75rcsBWACqYwiIPHQ==")
+                    .setKeywords(keywords)
+                    .build(context, wakeWordCallback);
+
+            porcupineManager.start();
+        } catch (PorcupineException e) {
+            e.printStackTrace(); // Handle the exception as needed
+        }
+
+    }
+
+
+    private void stopWakeWordDetection() {
+        if (porcupineManager != null) {
+            try {
+                porcupineManager.stop();
+                porcupineManager.delete();
+            } catch (PorcupineException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    private PorcupineManagerCallback wakeWordCallback = new PorcupineManagerCallback() {
+        @Override
+        public void invoke(int keywordIndex) {
+            SharedPreferences sharedPreferences = getSharedPreferences("my_prefs", MODE_PRIVATE);
+            String selectedWakeWord = sharedPreferences.getString(Constants.TRIGGER_WORD_KEY, Constants.DEFAULT_TRIGGER_WORD);
+
+            Intent intent = new Intent("WAKE_WORD_DETECTED");
+            intent.putExtra("keywordIndex", keywordIndex);
+            intent.putExtra("selectedWakeWord", selectedWakeWord);
+            sendBroadcast(intent);
+
+        }
+    };
+
+
+}
+
+
