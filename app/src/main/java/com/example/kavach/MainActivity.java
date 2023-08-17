45a79d091836fb1658ae3ff098c8a287216c8fe5
vidit sharma <vidit.awesome2305@gmail.com>
2023-08-18 00:44:08 +0530

    Manual 1st draft Completed.

--- app/src/main/java/com/example/kavach/MainActivity.java
+++ app/src/main/java/com/example/kavach/MainActivity.java
@@ -79,7 +79,7 @@ public class MainActivity extends AppCompatActivity implements LocationHandler.L
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_settings, R.id.nav_tutorial, R.id.nav_about)
+                R.id.nav_home, R.id.nav_manual, R.id.nav_tutorial, R.id.nav_about)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
--- app/src/main/java/com/example/kavach/ui/manual/ManualFragment.java
+++ app/src/main/java/com/example/kavach/ui/manual/ManualFragment.java
@@ -1,27 +1,23 @@
-package com.example.kavach.ui.settings;
+package com.example.kavach.ui.manual;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.kavach.R;
-import com.example.kavach.databinding.FragmentHomeBinding;
-import com.example.kavach.ui.home.HomeViewModel;
 
-public class SettingsFragment extends Fragment {
+public class ManualFragment extends Fragment {
 
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                                  ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_settings,container,false);
+        return inflater.inflate(R.layout.fragment_manual,container,false);
     }
 
 }
