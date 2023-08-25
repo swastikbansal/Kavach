725a496cde6f91a607b03b791c7391a9ef13078b
Vihaan Vijayvargiya <vihaanvijayvargiya@gmail.com>
2023-08-25 23:20:32 +0530

    SmS reciever added and working in foreground with audio

--- app/src/main/java/com/example/kavach/SmsReceiver.java
+++ app/src/main/java/com/example/kavach/SmsReceiver.java
@@ -3,6 +3,7 @@ package com.example.kavach;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
@@ -21,10 +22,22 @@ public class SmsReceiver extends BroadcastReceiver {
                     // Compare the messageBody with your desired string and take action accordingly.
                     if (messageBody.equals("I am in an Emergency Situation. I need Help.")) {
                         Toast.makeText(context, "SmS confirmed", Toast.LENGTH_SHORT).show();
+                        playAudio(context);
                     }
                 }
             }
         }
+    }
+    private void playAudio(Context context) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.test); // Replace with your audio file
+        mediaPlayer.start();
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mediaPlayer.release();
+            }
+        });
     }
 }
 
